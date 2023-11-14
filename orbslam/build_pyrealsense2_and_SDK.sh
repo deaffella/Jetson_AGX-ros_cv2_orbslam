6330ff997a1579a8372a2869f8ee8463d92f0ede
deaffella <o1prime@yandex.ru>
2023-11-14 16:23:51 +0300

    CHANGE: + librealsense2 install script;

---
+++ orbslam/build_pyrealsense2_and_SDK.sh
@@ -0,0 +1,40 @@
+#   Author: Yavuz Selim Ozkaya
+# LinkedIn: https://www.linkedin.com/in/yavuzselimozkaya
+
+# References: https://github.com/IntelRealSense/librealsense/tree/master/wrappers/python
+# 	      https://github.com/IntelRealSense/librealsense/issues/6964
+#	      https://www.youtube.com/watch?v=EeT-pzM8n-o
+
+cd ~
+git clone https://github.com/IntelRealSense/librealsense.git
+cd librealsense && mkdir build && cd build
+sudo apt-get -y install python3 python3-dev libssl-dev libxinerama-dev libxcursor-dev libcanberra-gtk-module libcanberra-gtk3-module
+echo "[INFO] pyrealsense2 bindings build has been started!"
+sleep 2
+# Give the CUDA path to CMake
+sed -i '3iset(CMAKE_CUDA_COMPILER /usr/local/cuda/bin/nvcc)\' ../CMakeLists.txt
+# Set the flags so that librealsense is compatible with python
+cmake ../ -DBUILD_PYTHON_BINDINGS:bool=true \
+          -DPYTHON_EXECUTABLE="/usr/bin/python3" \
+          -DPYTHON_EXECUTABLE=/usr/bin/python3 \
+          -DPYTHON_INSTALL_DIR=$(python3 -c 'import sys; print(f"/usr/lib/python{sys.version_info.major}.{sys.version_info.minor}/dist-packages")') \
+          -DCMAKE_BUILD_TYPE=release \
+          -DBUILD_EXAMPLES=true \
+          -DBUILD_GRAPHICAL_EXAMPLES=true \
+          -DBUILD_WITH_CUDA:bool=true \
+          -DBUILD_NETWORK_DEVICE=ON
+          #-DFORCE_RSUSB_BACKEND=true
+          #-DOpenGL_GL_PREFERENCE
+
+sudo make uninstall && sudo make clean
+echo "[INFO] Building is starting, it will take a long time like half an hour or more!"
+sleep 2
+sudo make -j$(($(nproc)-1)) && sudo make install
+
+echo 'PYTHONPATH="/usr/local/lib:/usr/local/lib/python3.6/pyrealsense2:$PYTHONPATH"' >> ~/.bashrc
+echo 'export PYTHONPATH' >> ~/.bashrc
+
+sudo cp ~/librealsense/config/99-realsense-libusb.rules /etc/udev/rules.d/ && sudo udevadm control --reload-rules && udevadm trigger
+
+echo "[INFO] pyrealsense2 and RealSense SDK are ready to use!"
+echo '[INFO] pyrealsense2 can only be used with "python3" not with "python"!'
