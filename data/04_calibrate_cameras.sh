2c3a643303e3ae33136f2d1d1e22765f66410a1e
deaffella <o1prime@yandex.ru>
2023-11-15 17:02:12 +0300

    CHANGE: + update 04_calibrate_cameras.sh to stable;

--- data/04_calibrate_cameras.sh
+++ data/04_calibrate_cameras.sh
@@ -5,16 +5,17 @@ script_name=kalibr_calibrate_cameras
 
 data_dir_path=/data/calib_data
 bag_path=$data_dir_path/recorder.bag
-#april_yaml_path=$data_dir_path/april_6x6_80x80cm_larues.yaml
-april_yaml_path=$data_dir_path/april_to_be_updated.yaml
+
+april_yaml_path=$data_dir_path/april_6x6_80x80cm_larues.yaml
+#april_yaml_path=$data_dir_path/april_to_be_updated.yaml
 
 topics=/cam0/image_raw
 models=pinhole-radtan
 
 
 cd $script_dir && \
-./$script_name \
+python $script_name \
 --bag $bag_path \
 --topics $topics \
 --models $models \
---target pinhole-radtan \
+--target $april_yaml_path
